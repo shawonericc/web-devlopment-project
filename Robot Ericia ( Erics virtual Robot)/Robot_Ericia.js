9897ba6ec09329a10bad275253e76c104e092892
shawon eric <81037496+shawonericc@users.noreply.github.com>
2021-04-07 00:39:18 +0600

    Add files via upload

---
+++ Robot Ericia ( Erics virtual Robot)/Robot_Ericia.js
@@ -0,0 +1,2 @@
+alert("Ask him anything 😳..... Just don't touch him 😵 & PLZ!!! dont press another Button when One is Running");
+alert("version: 1.3 _ ShawonEric ~ 2nd Screen added on PRESSING 'CLEAR' button!")
